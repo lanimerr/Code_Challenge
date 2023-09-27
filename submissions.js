c74a0df40abf506b63d91b8bbccc872877a78110
MILANI <144742496+lanimerr@users.noreply.github.com>
2023-09-27 14:08:07 -0400

    Update submissions.js

--- submissions.js
+++ submissions.js
@@ -1,36 +1,27 @@
-//Problem 1
-
-const findSum = function(array){
-    let sum = 0;
+const findSum = function(array) {
+    let number = 0;
 
     for (let i = 0; i < array.length; i ++){
-        sum += array[i];
+        number += array[i];
     }
-    return sum
-};
- 
-//Problem 2
+    return number
+  };
+  
+  
+  const findFrequency = function(array) {
+    // your code here - don't forget to return an object!
+  };
 
-
-
-
-// Problem 3
-
-
-const isPalindrome = function(str){
+  
+  const isPalindrome = function(str) {
     let lowerCase = str.toLowerCase();
     let reverse = lowerCase.split('').reverse().join('');
     
     return(reverse  === lowerCase);
+  };
 
-};
-
-
-
-//Problem 4
-
-
-const largestPair = function(array){
+  
+  const largestPair = function(array) {
     let pair = 0;
     let num2 = 0;
     for (i = 0; i < array.length; i ++){
@@ -40,23 +31,17 @@ const largestPair = function(array){
         }
     }
     return pair;
-};
+  };
 
-
-//Problem 5
-
-
-const removeParenth = function (str) {
+  
+  const removeParenth = function(str) {
     const stringOne = str.indexOf("(");
     const stringTwo = str.indexOf(")");
-    
     return str.substring(0, stringOne) + str.substring(stringTwo + 1);
   };
-     
-
-//Problem 6
-
-const scoreScrabble = function(str){
+  
+  
+  const scoreScrabble = function(str) {
     let num = 0;
 
     for (let i = 0; i < str.length; i++){
@@ -116,5 +101,5 @@ const scoreScrabble = function(str){
     };
 
     return num;
-};
+  };
     
